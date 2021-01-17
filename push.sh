89559f9ffbfd1681ae08dff659a8d9c883f91c81
Mustafa Bozkurt <mwolf845300@gmail.com>
2021-01-17 02:01:04 +0100

    app.py

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 
 
 rm ./app.py
-cp /c/Users/mwolf/PycharmProjects/streamlit-machine-learning/automated-machine-learning.py .
+cp /c/Users/mwolf/PycharmProjects/streamlit-machine-learning/automated-machine-learning.py ./app.py
 git add .
 git commit -m "app.py"
 
