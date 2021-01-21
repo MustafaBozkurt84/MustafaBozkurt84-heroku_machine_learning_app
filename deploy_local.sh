a967b603cc5987ab80a6732bfdcc20d0f8b0fdee
Mustafa Bozkurt <mwolf845300@gmail.com>
2021-01-21 22:08:44 +0100

    app.py

--- deploy_local.sh
+++ deploy_local.sh
@@ -4,7 +4,7 @@ mkdir ./local_deployment/
 cp ./Procfile ./local_deployment/
 cp ./setup.sh ./local_deployment/
 cp ./local.sh  ./local_deployment/
-cp ./f-marshal.py ./local_deployment/
+cp ./f-marshal.py ./local_deployment/app.py
 cp *.pkl ./local_deployment/
 cp requirements_test.txt ./local_deployment/requirements.txt
 tar -cf local_deployment.tar ./local_deployment
